Kyuofox/tc-build

kernel/build.sh: Use personal binutils installation instead of default one
Signed-off-by: Nathan Chancellor <nathan@kernel.org>
Signed-off-by: KyuoFoxHuyu <88403766@qq.com>

kernel/build.sh
@@ -72,7 +72,7 @@ function set_default_values() {
 
 function setup_up_path() {
     # Add the default install bin folder to PATH for binutils
-    export PATH=${TC_BLD}/install/bin:${PATH}
+    export PATH=${CBL_BNTL:-${TC_BLD}/install/bin}:${PATH}
     # Add the stage 2 bin folder to PATH for the instrumented clang if we are doing PGO
     ${PGO:=false} && export PATH=${BUILD_FOLDER:=${TC_BLD}/build/llvm}/stage2/bin:${PATH}
     # If the user wants to add another folder to PATH, they can do it with the PATH_OVERRIDE variable

TianWalkzzMiku/my-script-1

kernel/build.sh
@@ -74,7 +74,7 @@ function set_default_values() {
 
 function setup_up_path() {
     # Add the default install bin folder to PATH for binutils
-    export PATH=${TC_BLD}/install/bin:${PATH}
+    export PATH=${CBL_TC_BNTL:-${TC_BLD}/install/bin}:${PATH}
     # Add the stage 2 bin folder to PATH for the instrumented clang if we are doing PGO
     ${PGO:=false} && export PATH=${BUILD_FOLDER:=${TC_BLD}/build/llvm}/stage2/bin:${PATH}
     # If the user wants to add another folder to PATH, they can do it with the PATH_OVERRIDE variable